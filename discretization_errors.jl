a47c13396de8a8397a89e4027181e5c4a3b8e30f
Petr Krysl <pkrysl@ucsd.edu>
2023-12-01 09:23:44 -0800

    delete unused implementations

--- discretization_errors.jl
+++ discretization_errors.jl
@@ -3,41 +3,9 @@ using FRFComparisons
 using Statistics
 
 
-function collect_summaries(n1, n2)
-    all_sensors = [
-    FRFPlots.TPC_L_transverse,
-    FRFPlots.TPC_L_axial,
-    FRFPlots.TPC_R_transverse,
-    FRFPlots.TPC_R_axial,
-    FRFPlots.Skull_transverse,
-    FRFPlots.Skull_axial,
-    FRFPlots.Skull_normal,
-    ]
-    f = frequencies(n1)
-
-    cssf_result = []
-    csac_result = []
-    frfsm_result = []
-    for c in all_sensors
-        f = frequencies(n1)
-        d1 = measurements(n1, c)
-        f = frequencies(n2)
-        d2 = measurements(n2, c)
-        push!(cssf_result, cssf(f, d1, d2))
-        push!(csac_result, csac(f, d2, d1))
-        push!(frfsm_result, frfsm(f, d2, d1))
-    end
-
-    cssf_result, csac_result, frfsm_result
-end
-
-function stats(v)
-    mean(v), stdm(v, mean(v))
-end
-
 cssf_result, csac_result, frfsm_result = collect_summaries(
-    "../plots_sim/gw_fs_plastic_transdec_E=2_1_m=13_nev=400_rho=1080_a=0_lf=0_05_sw=1k.csv",
-    "../plots_sim/gw_fs_plastic_transdec_E=2_1_m=10_nev=400_rho=1080_a=0_lf=0_05_sw=1k.csv"
+    "../plots_sim/gw_fs_plastic_transdec_E=1_74_m=13_nev=400_rho=1008_a=0_lf=0_05_sw=1k.csv",
+    "../plots_sim/gw_fs_plastic_transdec_E=1_74_m=10_nev=400_rho=1008_a=0_lf=0_05_sw=1k.csv"
     )
 @show stats(cssf_result)
 @show stats(csac_result)
@@ -45,16 +13,16 @@ cssf_result, csac_result, frfsm_result = collect_summaries(
 
 
 cssf_result, csac_result, frfsm_result = collect_summaries(
-    "../plots_sim/gw_fs_plastic_transdec_E=2_1_m=10_nev=400_rho=1080_a=0_lf=0_05_sw=1k.csv",
-    "../plots_sim/gw_fs_plastic_transdec_E=2_1_m=6_nev=400_rho=1080_a=0_lf=0_05_sw=1k.csv"
+    "../plots_sim/gw_fs_plastic_transdec_E=1_74_m=10_nev=400_rho=1008_a=0_lf=0_05_sw=1k.csv",
+    "../plots_sim/gw_fs_plastic_transdec_E=1_74_m=6_nev=400_rho=1008_a=0_lf=0_05_sw=1k.csv"
     )
 @show stats(cssf_result)
 @show stats(csac_result)
 @show stats(frfsm_result)
 
 cssf_result, csac_result, frfsm_result = collect_summaries(
-    "../plots_sim/gw_fs_plastic_transdec_E=2_1_m=6_nev=400_rho=1080_a=0_lf=0_05_sw=1k.csv",
-    "../plots_sim/gw_fs_plastic_transdec_E=2_1_m=3_nev=400_rho=1080_a=0_lf=0_05_sw=1k.csv"
+    "../plots_sim/gw_fs_plastic_transdec_E=1_74_m=6_nev=400_rho=1008_a=0_lf=0_05_sw=1k.csv",
+    "../plots_sim/gw_fs_plastic_transdec_E=1_74_m=3_nev=400_rho=1008_a=0_lf=0_05_sw=1k.csv"
     )
 @show stats(cssf_result)
 @show stats(csac_result)
--- go.jl
+++ go.jl
@@ -12,9 +12,9 @@ FRFPlots.Skull_axial,
 
 
 
-ax = FRFPlots.frfplots("../simulations/gw_fs_plastic_transdec_E=2_1_m=10_nev=400_rho=1080_a=0_lf=0_05_sw=1k.csv",
-    all)
-display(ax)
+# ax = FRFPlots.frfplots("../simulations/gw_fs_plastic_transdec_E=2_1_m=10_nev=400_rho=1080_a=0_lf=0_05_sw=1k.csv",
+#     all)
+# display(ax)
 
 # ax = FRFPlots.frfplots("../plots_TRANSDEC_28Aug2023/AccelOverPressureMs2Pa_RecordNI_amps_TRANSDEC_28Aug2023_0deg_run2.csv",
 #     all)
@@ -37,5 +37,41 @@ display(ax)
 # # csac(f, d1, d2)
 #     @show frfsm(f, d2, d1)
 # end
+
+data1 = "../plots_sim/gw_fs_plastic_transdec_E=1_74_m=3_nev=400_rho=1080_a=0_lf=0_05_sw=1k.csv"
+data2 = "../plots_sim/gw_fs_plastic_transdec_E=2_1_m=3_nev=400_rho=1080_a=0_lf=0_05_sw=1k.csv"
+
+data1 = "../plots_sim/pp_E=1_74_m=13_nev=400_rho=1008_a=0_lf=0_05_sw=1k.csv"
+data2 = "../plots_sim/pp_E=1_74_m=13_nev=400_rho=1008_a=0_lf=0_05_sw=1kalt.csv"
+
+ax = FRFPlots.frfplots([
+    (data1, FRFPlots.TPC_L_transverse, "M1"),
+    (data2, FRFPlots.TPC_L_transverse, "E"),
+    ])
+display(ax)
+savepdf("TPC_L_transverse" * ".pdf", ax)
+
+
+ax = FRFPlots.frfplots([
+    (data1, FRFPlots.TPC_R_transverse, "M1"),
+    (data2, FRFPlots.TPC_R_transverse, "E"),
+    ])
+display(ax)
+savepdf("TPC_R_transverse" * ".pdf", ax)
+
+ax = FRFPlots.frfplots([
+    (data1, FRFPlots.Skull_transverse, "M1"),
+    (data2, FRFPlots.Skull_transverse, "E"),
+    ])
+display(ax)
+savepdf("Skull_transverse", ax)
+
+ax = FRFPlots.frfplots([
+    (data1, FRFPlots.Skull_axial, "M1"),
+    (data2, FRFPlots.Skull_axial, "E"),
+    ])
+display(ax)
+savepdf("Skull_axial", ax)
+
 
 nothing
